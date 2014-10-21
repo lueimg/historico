b44bc3d007f315a8cadc7e4b03a7b3213aca168a
jorge <jorge@salcedo.com>
2014-10-21 11:59:20 -0500

    consultacritico

--- consultaCritico.php
+++ consultaCritico.php
@@ -30,6 +30,33 @@ input[type="text"], select {
 }
 </style>
 
+<style>
+
+.celda0 {
+	background-color: #6E9CC8; padding: 3px;
+	color: #FFFFFF;
+	border: 1px solid #17688B;
+	font-size: 11px;
+}
+
+
+.celda1 {
+	background-color: white; padding: 3px;
+	color: #2A4266;
+	border: 1px solid #6297BC;
+	font-size: 11px;
+}
+
+.celda2 {
+	background-color: white; padding: 3px;
+	color: #2A4266;
+	border: 1px solid #6297BC;
+	font-size: 10px;
+	font: Verdana,Arial,Helvetica,sans-serif;
+}
+
+</style>
+
 <script type="text/javascript">
 	
 	$(document).ready(function(){
@@ -41,6 +68,9 @@ input[type="text"], select {
 	
  
 		$("#btnBuscar").click(function(){
+
+			$("#resultado1").html("");
+
 			if ($("#txtBus").val()=='') {
 				alert("Debe ingresar un dato para buscar.");
 				return false;
@@ -50,13 +80,14 @@ input[type="text"], select {
 			$.ajax({
 				type: "POST",
 				url: pagina,
+				dataType: "Json",
 				data: {
 					filtro: 'filtro_personalizado',
-					cmbTipo: $("#cmbTipo").val(),
-					txtBus: $("#txtBus").val()
+					tipo: $("#cmbTipo").val(),
+					valor_filtro: $("#txtBus").val()
 				},
-				success: function(html){
-					$("#resultado1").html(html);
+				success: function(obj){
+					listarResultado(obj);
 				}
 			});			
 
@@ -66,15 +97,38 @@ input[type="text"], select {
 
 	});
 
-cargarDetalle=function(){
+listarResultado=function(datos){
+	var htm="";
+	$("#div_listado").css("display","");
+	$("#tabla_listado .elimina").remove();
+	if(datos.length>0){
+		$.each(datos,function(index,data){
+			htm='';
+			htm='<tr class="elimina">'+
+					'<td class="celda2" onClick="cargarDetalle('+data.id+');"><img title="Mostrar Detalle" alt="Mostrar Detalle" src="img/mov.jpg"></td>'+
+			    	'<td class="celda2">'+data.averia+'</td>'+
+			    	'<td class="celda2">'+data.id_atc+'</td>'+
+			    	'<td class="celda2">'+data.tipo_actividad+'</td>'+
+			    	'<td class="celda2">'+data.fecha_reg+'</td>'+
+			    	'<td class="celda2">'+data.quiebres+'</td>'+
+			    	'<td class="celda2">'+data.empresa+'</td>'+
+			    	'<td class="celda2">'+data.estado+'</td>'+
+			    	'<td class="celda2">'+data.fecha_agenda+' '+data.horario+'</td>'+
+			    	'<td class="celda2">'+data.tecnico+'</td>'+
+			    '</tr>';
+			$("#tabla_listado").append(htm);
+		});
+	}	
+}
+
+cargarDetalle=function(idg){
 var pagina="consultaCritico_ajax.php";
 	$.ajax({
 		type: "POST",
 		url: pagina,
 		data: {
 			action: 'buscar',
-			cmbTipo: $("#cmbTipo").val(),
-			txtBus: $("#txtBus").val()
+			id:idg
 		},
 		success: function(html){
 			$("#resultado1").html(html);
@@ -84,16 +138,16 @@ var pagina="consultaCritico_ajax.php";
 
 validarTexto=function(valor){
 	$("#txtBus").val('');
-	if(valor=="1"){		
+	if(valor=="telefono"){		
 		$("#txtBus").attr("onKeyPress","return validaNumeros(event);");		
 	}
-	else if(valor=="2" || valor=="3"){
+	else if(valor=="averia" || valor=="atc"){
 		$("#txtBus").attr("onKeyPress","return validaAlfanumerico(event);");
 	}
 	/*else if(valor=="4"){
 		$("#txtBus").attr("onKeyPress","return validaLetras(event);");
 	}*/
-	else if(valor=='5'){
+	else if(valor=='id_gestion'){
 		$("#txtBus").attr("onKeyPress","return validaNumerosIn(event);");
 	}
 	$("#txtBus").focus();
@@ -165,10 +219,10 @@ validaAlfanumerico=function(e) { // 1
 			<td style="width: 15%">Tipo Busqueda:</td >
 			<td style="width: 10%; background-color:white; padding: 3px;" >
 				<select name='cmbTipo' id='cmbTipo' onchange="validarTexto(this.value);" >					
-					<option value='1'>&nbsp;Telefono</option>
-					<option value='2'>&nbsp;Averia</option>
-					<option value='3'>&nbsp;ATC</option>
-					<option value='5'>&nbsp;Id Gestion</option>
+					<option value='telefono'>&nbsp;Telefono</option>
+					<option value='averia'>&nbsp;Averia</option>
+					<option value='atc'>&nbsp;ATC</option>
+					<option value='id_gestion'>&nbsp;Id Gestion</option>
 				</select>
 			</td >
 			<td style="width: 75%; background-color:white; padding: 3px;"><input type='text' name='txtBus' id='txtBus' onkeyup="return validaEnter(event);" />
@@ -182,24 +236,28 @@ validaAlfanumerico=function(e) { // 1
 
 <div id="register"></div>
 
-<div id="div_listado" class="divBusqueda" style="width: 750px" >
-	<table class="tablaBusqueda" style='width: 90%'>
+<div id="div_listado" class="divBusqueda" style="width: 1000px">
+	<table id="tabla_listado" class="tablaBusqueda" cellpadding="1" cellspacing="0" border="1">
 		<thead>
-			<th colspan='7'>RESULTADOS DE LA BUSQUEDA</th>
+			<th colspan='10'>RESULTADOS DE LA BUSQUEDA</th>
 		</thead>
 		<tr class="tr_busqueda">
-			<th class="celda0" style='text-align:center'>Fecha Movimiento</th >
-			<th class="celda0" style='text-align:center'>Usuario</th >
-			<th class="celda0" style='text-align:center'>Estado</th >
-			<th class="celda0" style='text-align:center'>Observaciones</th >
-			<th class="celda0" style='text-align:center'>Motivo</th >
-			<th class="celda0" style='text-align:center'>Submotivo</th >	
-			<th class="celda0" style='text-align:center'>Fecha Agenda / Turno</th >			
+			<th style='text-align:center' width="50px">[ ]</th >
+			<th style='text-align:center' width="80px">Averia</th >
+			<th style='text-align:center' width="80px">Cod Atencion</th >
+			<th style='text-align:center' width="80px">Tipo Actividad</th >
+			<th style='text-align:center' width="130px">Fecha Reg</th >
+			<th style='text-align:center' width="80px">Quiebre</th >
+			<th style='text-align:center' width="80px">Empresa</th >
+			<th style='text-align:center' width="70px">Estado</th >
+			<th style='text-align:center' width="130px">Fecha Agenda</th >	
+			<th style='text-align:center' width="230px">Tecnico</th >			
 		</tr>		
 		
 	</table>
 </div>
-	
+	<br>
+	<br>
 <div id="resultado1"></div>
 
 </body>
--- consultaCritico_ajax.php
+++ consultaCritico_ajax.php
@@ -8,100 +8,72 @@ header("Pragma: no-cache");
 
 include ("../../clases/class.Conexion.php");
 
-$objCnx = new Conexion();
+ $db = new Conexion();
+ $cnx = $db->conectarPDO();
 
 
 if ($_POST['action']=='buscar')
 {
-	$cnx = $objCnx->conectarBD();
-	
-	$cmbTipo = $_POST["cmbTipo"];
-	$txtBus = $_POST["txtBus"];
-	
-	switch($cmbTipo) {
-		case 1:
-			// Por telefono	
-			$filtroBusqueda = " AND telefono='$txtBus' ";
-			break;
-		case 2:
-			// Por Averia	
-			$filtroBusqueda = " AND averia='$txtBus' ";
-			break;
-	}
+	$id=$_POST["id"];
 
-
-	$cad1 = "SELECT * FROM webpsi_criticos.`gestion_averia` WHERE 1=1 $filtroBusqueda LIMIT 1";
-	//echo $cad1;
-	$res1 = mysql_query($cad1) or die("Error 1: ".mysql_error());
-	$res1_results = mysql_num_rows($res1); 
-	//echo $res1_results;
-	
-	if ($res1 && $res1_results>0 ) {
-		$arrAveria = mysql_fetch_array($res1, MYSQL_ASSOC);
-	
-
-		//var_dump($arrAveria);
-		
-		$idGestion = $arrAveria["id_gestion"];
-		//echo "Idgestion = ".$idGestion;
-		
-		$cad2 = "SELECT a.*, e.estado, h.horario FROM webpsi_criticos.`gestion_criticos` a, webpsi_criticos.estados e, webpsi_criticos.horarios h
-				WHERE a.id=$idGestion AND e.id=a.id_estado AND h.id=a.id_horario LIMIT 1";
-		$res2 = mysql_query($cad2) or die("Error 2: ".mysql_error());
-		
-		$arrCabecera = mysql_fetch_array($res2, MYSQL_ASSOC);
+		$cnx->exec("set names utf8");
+		$cad2 = "SELECT a.*, e.estado, h.horario, av.averia, av.fecha_registro 
+				 FROM webpsi_criticos.`gestion_criticos` a
+				 INNER JOIN webpsi_criticos.gestion_averia av ON av.id_gestion=a.id
+				 INNER JOIN webpsi_criticos.estados e ON e.id=a.id_estado
+				 INNER JOIN webpsi_criticos.horarios h ON h.id=a.id_horario
+				 WHERE a.id=$id 
+				 UNION ALL
+				 SELECT a.*, e.estado, h.horario, av.averia, av.fecha_registro 
+				 FROM webpsi_criticos.`gestion_criticos` a
+				 INNER JOIN webpsi_criticos.gestion_rutina_manual av ON av.id_gestion=a.id
+				 INNER JOIN webpsi_criticos.estados e ON e.id=a.id_estado
+				 INNER JOIN webpsi_criticos.horarios h ON h.id=a.id_horario
+				 WHERE a.id=$id 
+				 UNION ALL
+				 SELECT a.*, e.estado, h.horario, av.codigo_req averia, av.fecha_Reg fecha_registro 
+				 FROM webpsi_criticos.`gestion_criticos` a
+				 INNER JOIN webpsi_criticos.gestion_provision av ON av.id_gestion=a.id
+				 INNER JOIN webpsi_criticos.estados e ON e.id=a.id_estado
+				 INNER JOIN webpsi_criticos.horarios h ON h.id=a.id_horario
+				 WHERE a.id=$id
+				 UNION ALL
+				 SELECT a.*, e.estado, h.horario, av.codigo_req averia, av.fecha_Reg fecha_registro 
+				 FROM webpsi_criticos.`gestion_criticos` a
+				 INNER JOIN webpsi_criticos.gestion_rutina_manual_provision av ON av.id_gestion=a.id
+				 INNER JOIN webpsi_criticos.estados e ON e.id=a.id_estado
+				 INNER JOIN webpsi_criticos.horarios h ON h.id=a.id_horario
+				 WHERE a.id=$id";
+		$res = $cnx->query($cad2);
+        $arrCabecera=$res->fetch(PDO::FETCH_ASSOC);
+          
 		
 		$cad3 = "SELECT a.*, e.estado, h.horario, m.motivo, m2.submotivo, u.usuario,
 				 date_format(a.fecha_movimiento, '%d-%m-%Y %H:%i:%s') as fecha_mov2,
 				 date_format(a.fecha_agenda, '%d-%m-%Y') as fecha_agenda2
-				 FROM webpsi_criticos.`gestion_movimientos` a, webpsi_criticos.estados e, webpsi_criticos.horarios h,
-				 webpsi.tb_usuario u,
-				 webpsi_criticos.motivos m, webpsi_criticos.submotivos m2
-				 WHERE id_gestion=1393 AND e.id=a.id_estado AND h.id=a.id_horario 
-				 AND m.id=a.id_motivo AND m2.id=a.id_submotivo
-				 AND u.id=a.id_usuario
+				 FROM webpsi_criticos.`gestion_movimientos` a
+				 INNER JOIN webpsi_criticos.estados e ON e.id=a.id_estado 
+				 INNER JOIN webpsi_criticos.horarios h ON h.id=a.id_horario 
+				 INNER JOIN webpsi.tb_usuario u ON u.id=a.id_usuario 
+				 INNER JOIN webpsi_criticos.motivos m ON m.id=a.id_motivo 
+				 INNER JOIN webpsi_criticos.submotivos m2 ON m2.id=a.id_submotivo
+				 WHERE id_gestion=$id  				 
 				 ORDER BY fecha_movimiento DESC ";
-		$res3 = mysql_query($cad3) or die(mysql_error());
-		
-		while ($row3 = mysql_fetch_array($res3, MYSQL_ASSOC))
-			$arrMovimientos[] = $row3;
+		$res = $cnx->query($cad3);
+        while ($row = $res->fetch(PDO::FETCH_ASSOC)) {
+            $arrMovimientos[] = $row;
+        }
+        $cnx = NULL;
+        $db = NULL;
 		
 		//var_dump($arrCabecera);
 		//var_dump($arrMovimientos);
 		
 		//echo "1";
-		
+	if(count(arrMovimientos)>0){
 		?>
 		
 
-<style>
-
-.celda0 {
-	background-color: #6E9CC8; padding: 3px;
-	color: #FFFFFF;
-	border: 1px solid #17688B;
-	font-size: 11px;
-}
-
-
-.celda1 {
-	background-color: white; padding: 3px;
-	color: #2A4266;
-	border: 1px solid #6297BC;
-	font-size: 11px;
-}
-
-.celda2 {
-	background-color: white; padding: 3px;
-	color: #2A4266;
-	border: 1px solid #6297BC;
-	font-size: 10px;
-	font: Verdana,Arial,Helvetica,sans-serif;
-}
-
-</style>
-
-		
 		<div id="div_bus" class="divBusqueda" style="width: 750px" >
 
 			<table class="tablaBusqueda" style='width: 90%'>
@@ -111,14 +83,14 @@ if ($_POST['action']=='buscar')
 			
 				<tr class="tr_busqueda">
 					<td class="celda0">ATC:</td ><td class="celda1"><?php echo $arrCabecera["id_atc"]?></td>
-					<td class="celda0">Averia:</td ><td class="celda1"><?php echo $arrAveria["averia"]?></td>
+					<td class="celda0">Averia:</td ><td class="celda1"><?php echo $arrCabecera["averia"]?></td>
 				</tr>
 				<tr>
 					<td class="celda0">Nombre del cliente:</td >
 					<td class="celda1" colspan='3'><?php echo ucwords(strtolower($arrCabecera["nombre_cliente_critico"]))?></td>
 				</tr>
 				<tr class="tr_busqueda">
-					<td class="celda0">Fecha registro averia:</td ><td class="celda1"><?php echo $arrAveria["fecha_registro"]?></td>
+					<td class="celda0">Fecha registro averia:</td ><td class="celda1"><?php echo $arrCabecera["fecha_registro"]?></td>
 					<td class="celda0">Fecha creacion ATC:</td ><td class="celda1"><?php echo $arrCabecera["fecha_creacion"]?></td>
 				</tr>	
 				<tr class="tr_busqueda">
